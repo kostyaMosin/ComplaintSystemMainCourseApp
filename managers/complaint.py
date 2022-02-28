1a47c3e9823880b74fa0ad7cba180cb185b70074
Konstantin Mosin <konstantinmosin@coherentsolutions.com>
2022-02-28 16:34:09 +0300

    integrate wise payments

--- managers/complaint.py
+++ managers/complaint.py
@@ -4,13 +4,15 @@ import uuid
 from constants import TEMP_FILE_FOLDER
 from db import database
 from managers.user import UserManager
-from models import complaint, RoleType, State
+from models import complaint, RoleType, State, transaction
 from services.s3 import S3Service
 from services.ses import SESService
+from services.wise import WiseService
 from utils.helpers import decode_photo
 
 s3 = S3Service()
 ses = SESService()
+wise = WiseService()
 
 
 class ComplaintManager:
@@ -33,7 +35,11 @@ class ComplaintManager:
         decode_photo(path, encoded_photo)
         complaint_data["photo_url"] = s3.upload_photo(path, name, extension)
         os.remove(path)
-        id_ = await database.execute(complaint.insert().values(complaint_data))
+        async with database.transaction() as t_conn:
+            id_ = await t_conn._connection.execute(complaint.insert().values(complaint_data))
+            await ComplaintManager.issue_transaction(
+                t_conn, complaint_data["amount"], f"{user['first_name']} {user['last_name']}", user["iban"], id_
+            )
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
 
     @staticmethod
@@ -42,6 +48,8 @@ class ComplaintManager:
 
     @staticmethod
     async def approve(id_):
+        transaction_data = await database.fetch_one(transaction.select().where(transaction.c.complaint_id == id_))
+        wise.fund_transfer(transaction_data["transfer_id"])
         await database.execute(
             complaint.update().where(complaint.c.id == id_).values(status=State.approved)
         )
@@ -57,9 +65,25 @@ class ComplaintManager:
         await database.execute(
             complaint.update().where(complaint.c.id == id_).values(status=State.rejected)
         )
+        transaction_data = await database.fetch_one(transaction.select().where(transaction.c.complaint_id == id_))
+        wise.cancel_transfer(transaction_data["transfer_id"])
         complainer = await UserManager.get_user_by_complain_id(id_)
         ses.send_mail(
             "Complaint rejected!",
             [complainer["email"]],
-            "Your claim is rejected, for some reasons."
+            "Your claim is rejected. Call your approver to have more details."
         )
+
+    @staticmethod
+    async def issue_transaction(t_conn, amount, full_name, iban, complaint_id):
+        quote_id = wise.create_quote(amount)
+        recipient_id = wise.create_recipient_account(full_name, iban)
+        transfer_id = wise.create_transfer(recipient_id, quote_id)
+        data = {
+            "quote_id": quote_id,
+            "transfer_id": transfer_id,
+            "target_account_id": str(recipient_id),
+            "amount": amount,
+            "complaint_id": complaint_id
+        }
+        await t_conn._connection.execute(transaction.insert().values(**data))
